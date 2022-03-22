ff34f67c4bb7c1f377d7974575ed45501ed52d78
Nathan Sharick <sharick.nathan@gmail.com>
2022-03-22 16:55:56 -0500

    exercise updates

--- feature_engineering.py
+++ feature_engineering.py
@@ -11,16 +11,23 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, Qu
 
 #K Best - feature selection function
 def select_k_best(x, y, k):
+    #create the kbest object
     kbest = SelectKBest(f_regression, k=k)
+    #fit the kbest object with the input df from the function
     kbest.fit(x, y)
+    #output the results
     kbest_results = pd.DataFrame(dict(p=kbest.pvalues_, f=kbest.scores_), index=x.columns)
     print(f'The {k} best features = {list(x.columns[kbest.get_support()])}')
     return kbest_results
 
 #recursive feature elimination - feature selection function
 def rfe(x, y, n_features):
+    #create the model object
     model = LinearRegression()
+    #create the rfe object
     rfe = RFE(model, n_features_to_select=n_features)
+    #fit the rfe object
     rfe.fit(x, y)
+    #output the results
     print(f'The {n_features} best features = {list(x.columns[rfe.get_support()])}')
     return pd.DataFrame({'rfe_ranking': rfe.ranking_}, index=x.columns)
